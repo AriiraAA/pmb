d6d53ecaf47416041be40ff1cd3da79fdd61ca35
AriiraAA <arliansyah_azhary@yahoo.com>
2016-05-25 20:30:48 +0700

    Persiapan untuk pelatihan PMB

---
+++ delete_admin.php
@@ -0,0 +1,25 @@
+<?php 
+	session_start(); 
+	require_once "connect.php";
+	if (!isset($_SESSION['nim']) || !isset($_SESSION['nama']) || !isset($_SESSION['role'])) {
+		header("Location: login_admin.php");
+		exit;
+	} else {
+		$nim	= $_SESSION['nim'];
+		$nama	= $_SESSION['nama'];
+		$role	= $_SESSION['role'];
+	}
+
+	if ($_SESSION['role'] != "Super Admin") {
+		header("Location: admin.php");
+		exit;
+	}
+
+	if (!isset($_GET['admin_nim'])) {
+		header("Location: admin.php");
+		exit;
+	}
+
+	mysqli_query($connection, "DELETE FROM admin WHERE nim='".mysqli_real_escape_string($_GET['admin_nim'])."'");
+	header("Location: admin_list.php");
+?>
